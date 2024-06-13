ab820bb75ab020e456231dbaf57ce52452e2d811
pttoan23112002 <pttoan2311200221pm@gmail.com>
2024-06-13 17:17:18 +0700

    Lesson 11, 12: Object, Array Type

---
+++ projects/lesson11/lesson11.ts
@@ -0,0 +1,15 @@
+let person11: object;
+person11 = { name: 'Toan' };
+// person11 = 'Toan';
+
+let pro11: {
+    name: string,
+    age: number
+} = {
+    name: 'Ten',
+    age: 25
+}
+
+// pro11.address = 'Ha Noi';
+
+console.log(pro11);
---
+++ projects/lesson12/lesson12.ts
@@ -0,0 +1,8 @@
+//Array
+let test12 = ['Eric', 69, 1 == 1 ? true : false]; // Mixed types
+
+test12.push('Hoi dan IT');
+test12.push(100);
+test12.push(+'1' == 1);
+
+console.log('>check array:', test12);
