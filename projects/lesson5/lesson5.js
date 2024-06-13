e1e3352d5dfed1af3da90ab034e188689e77e681
pttoan23112002 <pttoan2311200221pm@gmail.com>
2024-06-13 14:57:57 +0700

    Lesson5 TypeScript Types

---
+++ projects/lesson5/lesson5.js
@@ -0,0 +1,3 @@
+var name = 'Toan';
+console.log('Length:', name.length);
+console.log('Upper case:', name.toUpperCase());
---
+++ projects/lesson5/lesson5.ts
@@ -0,0 +1,4 @@
+const nameTS: string = 'Toan';
+
+console.log('Length:', nameTS.length);
+console.log('Upper case:', nameTS.toUpperCase());
