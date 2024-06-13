42a41e27fc92298b4962795f86d2523839d081cd
pttoan23112002 <pttoan2311200221pm@gmail.com>
2024-06-13 17:31:50 +0700

    Lesson 13: Tuple Types

---
+++ projects/lesson13/lesson13.ts
@@ -0,0 +1,9 @@
+//Tuple Types : Array mở rộng
+let skills = ['Thanh Toan', 25];
+skills.push('coding');
+skills.push(100);
+
+//tuple : Kiểu dữ liệu, số lượng phần tử, sắp xếp theo thứ tự
+let skills1: [string, number] = ['Thanh Toan', 20];
+let skills2: [boolean, string, number?];
+skills2 = [true, 'name']
