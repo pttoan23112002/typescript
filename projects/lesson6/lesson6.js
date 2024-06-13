7c6c0ec98348997621485d0ca66e20921667da81
pttoan23112002 <pttoan2311200221pm@gmail.com>
2024-06-13 15:16:50 +0700

    Lesson 6: Type Annotations

---
+++ projects/lesson6/lesson6.js
@@ -0,0 +1,8 @@
+// Type Annotations - Chú thích
+var count = 1;
+// count = '10';
+console.log('Check count:', count);
+
+var names = ['Eric', 'Toan', 'Nhut']; // array
+// names.push(25);
+console.log('Array names:', names);
---
+++ projects/lesson6/lesson6.ts
@@ -0,0 +1,8 @@
+// Type Annotations - Chú thích
+let countTS: number = 1;
+// countTS = '10';
+console.log('Check countTS:', countTS);
+
+let namesTS: string[] = ['Eric', 'Toan', 'Nhut']; // array
+// namesTS.push(25);
+console.log('Array namesTS:', namesTS);
